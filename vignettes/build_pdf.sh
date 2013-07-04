e0b66b3419282561ba2367e232d6029e9e8a5081
snoweye <wccsnow@gmail.com>
2013-07-04 13:44:15 -0400

    finish migration

--- vignettes/build_pdf.sh
+++ vignettes/build_pdf.sh
@@ -11,3 +11,7 @@ rm *.aux *.bbl *.blg *.log *.out *.toc
 # mv phyclust-guide.pdf phyclust-guide.pdf.org
 # qpdf phyclust-guide.pdf.org phyclust-guide.pdf
 # rm *.org
+
+mv -f *.pdf ../inst/doc/
+cp -f *.Rnw ../inst/doc/
+cp -f *.html ../inst/doc/
