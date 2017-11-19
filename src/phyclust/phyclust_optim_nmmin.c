24ad7472e55691a5417bce1df7a943b83cbf3d2f
wccsnow <wccsnow@gmail.com>
2017-11-19 09:39:24 -0500

    updates

--- src/phyclust/phyclust_optim_nmmin.c
+++ src/phyclust/phyclust_optim_nmmin.c
@@ -167,7 +167,7 @@ int optim_nmmin(int n, double *Bvec, double *X, double *Fmin,
     double oldsize;
     double **P;
     double size, step, temp, trystep;
-    char tstr[10];
+    char tstr[12];
     double VH, VL, VR;
 
     if (maxit <= 0) {
