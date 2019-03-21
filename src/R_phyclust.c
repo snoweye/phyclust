7c1e9724d12fb78e297f6ffd6296ddbc3eeb3875
wccsnow <wccsnow@gmail.com>
2019-03-21 08:31:31 -0400

    Fix more rchk problem

--- src/R_phyclust.c
+++ src/R_phyclust.c
@@ -74,39 +74,39 @@ SEXP initialize_emptr(EMPTR emptr, phyclust_struct *pcs){
 	double *tmp_ptr_double;
 
 	/* Allocate and protect storages. */
-  	emobj = allocVector(VECSXP, emobj_length);
-  	emobj_names = allocVector(STRSXP, emobj_length);
-  	QA_names = allocVector(STRSXP, QA_length);
-  	converge_names = allocVector(STRSXP, converge_length);
+  	PROTECT(emobj = allocVector(VECSXP, emobj_length));
+  	PROTECT(emobj_names = allocVector(STRSXP, emobj_length));
+  	PROTECT(QA_names = allocVector(STRSXP, QA_length));
+  	PROTECT(converge_names = allocVector(STRSXP, converge_length));
 	
-  	N_X_org = allocVector(INTSXP, 1);
-  	N_X = allocVector(INTSXP, 1);
-  	L = allocVector(INTSXP, 1);
-  	K = allocVector(INTSXP, 1);
-  	Eta = allocVector(REALSXP, pcs->K);
-	Z_normalized = allocVector(REALSXP, pcs->N_X_org * pcs->K);
-  	Mu = allocVector(INTSXP, pcs->K * pcs->L);
-  	QA = allocVector(VECSXP, QA_length);
-	  	pi = allocVector(REALSXP, pcs->ncode * pcs->K);
-  		kappa = allocVector(REALSXP, 1 * pcs->K);
-  		Tt = allocVector(REALSXP, 1 * pcs->K);
-  	logL = allocVector(REALSXP, 1);
-  	p = allocVector(INTSXP, 1);
-  	bic = allocVector(REALSXP, 1);
-  	aic = allocVector(REALSXP, 1);
-  	icl = allocVector(REALSXP, 1);
-  	N_seg_site = allocVector(INTSXP, 1);
-  	class_id = allocVector(INTSXP, pcs->N_X_org);
-  	n_class = allocVector(INTSXP, pcs->K);
-  	converge = allocVector(VECSXP, converge_length);
-		converge_eps = allocVector(REALSXP, 1);
-		converge_error = allocVector(REALSXP, 1);
-		converge_flag = allocVector(INTSXP, 1);
-		converge_iter = allocVector(INTSXP, 1);
-		converge_inner_iter = allocVector(INTSXP, 1);
-		converge_cm_iter = allocVector(INTSXP, 1);
-		check_param = allocVector(INTSXP, 1);
-	label_method = allocVector(INTSXP, 1);
+  	PROTECT(N_X_org = allocVector(INTSXP, 1));
+  	PROTECT(N_X = allocVector(INTSXP, 1));
+  	PROTECT(L = allocVector(INTSXP, 1));
+  	PROTECT(K = allocVector(INTSXP, 1));
+  	PROTECT(Eta = allocVector(REALSXP, pcs->K));
+	PROTECT(Z_normalized = allocVector(REALSXP, pcs->N_X_org * pcs->K));
+  	PROTECT(Mu = allocVector(INTSXP, pcs->K * pcs->L));
+  	PROTECT(QA = allocVector(VECSXP, QA_length));
+	  	PROTECT(pi = allocVector(REALSXP, pcs->ncode * pcs->K));
+  		PROTECT(kappa = allocVector(REALSXP, 1 * pcs->K));
+  		PROTECT(Tt = allocVector(REALSXP, 1 * pcs->K));
+  	PROTECT(logL = allocVector(REALSXP, 1));
+  	PROTECT(p = allocVector(INTSXP, 1));
+  	PROTECT(bic = allocVector(REALSXP, 1));
+  	PROTECT(aic = allocVector(REALSXP, 1));
+  	PROTECT(icl = allocVector(REALSXP, 1));
+  	PROTECT(N_seg_site = allocVector(INTSXP, 1));
+  	PROTECT(class_id = allocVector(INTSXP, pcs->N_X_org));
+  	PROTECT(n_class = allocVector(INTSXP, pcs->K));
+  	PROTECT(converge = allocVector(VECSXP, converge_length));
+		PROTECT(converge_eps = allocVector(REALSXP, 1));
+		PROTECT(converge_error = allocVector(REALSXP, 1));
+		PROTECT(converge_flag = allocVector(INTSXP, 1));
+		PROTECT(converge_iter = allocVector(INTSXP, 1));
+		PROTECT(converge_inner_iter = allocVector(INTSXP, 1));
+		PROTECT(converge_cm_iter = allocVector(INTSXP, 1));
+		PROTECT(check_param = allocVector(INTSXP, 1));
+	PROTECT(label_method = allocVector(INTSXP, 1));
 
 	/* Set the elements and names. */
 	i = 0;
@@ -207,6 +207,7 @@ SEXP initialize_emptr(EMPTR emptr, phyclust_struct *pcs){
 
 	/* Do NOT call UNPROTECT() within this constructor!! */
 	// UNPROTECT(emptr->C_protect_length);
+	UNPROTECT(32);
 	return(emobj);
 } /* End of initialize_emptr(). */
 
